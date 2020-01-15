6b23a9d4c9faed745c05478f0b14a0a55405925c
strongestavenger <mikemutoro@gmail.com>
2020-01-15 15:39:45 +0300

    initialize repo

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// reactstrap components
+import {
+  Button,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip
+} from "reactstrap";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer">
+        <Container>
+          <Row>
+            <Col md="3">
+            </Col>
+            <Col md="3">
+                  <NavLink href="https://creative-tim.com/contact-us?ref=blkdsr-footer">
+                    Contact Me
+                  </NavLink>
+            </Col>
+            <Col md="3">
+            <p>   </p>
+            <p></p>
+              <div className="btn-wrapper profile">
+                <Button
+                  className="btn-icon btn-neutral btn-round btn-simple"
+                  color="default"
+                  href="https://www.linkedin.com/in/michael-mukalo-90417894"
+                  id="tooltip622135962"
+                  target="_blank"
+                >
+                  <i className="fab fa-linkedin" />
+                </Button>
+                <UncontrolledTooltip delay={0} target="tooltip622135962">
+                  find me on linkedin
+                </UncontrolledTooltip>
+                <Button
+                  className="btn-icon btn-neutral btn-round btn-simple"
+                  color="default"
+                  href="https://www.github.com/mutoromike/"
+                  id="tooltip230450801"
+                  target="_blank"
+                >
+                  <i className="fab fa-github" />
+                </Button>
+                <UncontrolledTooltip delay={0} target="tooltip230450801">
+                  find me on github
+                </UncontrolledTooltip>
+              </div>
+            </Col>
+            <Col md="3">
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/PageHeader/PageHeader.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+
+class PageHeader extends React.Component {
+  render() {
+    return (
+      <div className="page-header header-filter">
+        <div className="squares square1" />
+        <div className="squares square2" />
+        <div className="squares square3" />
+        <div className="squares square4" />
+        <div className="squares square5" />
+        <div className="squares square6" />
+        <div className="squares square7" />
+        <Container>
+          <div className="content-center brand">
+            <h1 className="h1-seo">Michael Mukalo</h1>
+            <h3 className="d-none d-sm-block">
+              A BackEnd Engineer with a touch of DevOps and FrontEnd
+            </h3>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default PageHeader;
